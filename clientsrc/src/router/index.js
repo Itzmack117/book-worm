49626ea9668688928fb582ad9f5ecbd6ee3d9268
Jacob Grow <jacobagrow@gmail.com>
2020-06-24 11:49:08 -0600

    ignore fake errors in router

--- clientsrc/src/router/index.js
+++ clientsrc/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//@ts-ignore
 import BookDetails from '../views/BookDetails.vue'
+//@ts-ignore
 import Inventory from "../views/Inventory.vue"
+//@ts-ignore
 import Order from '../views/Order.vue'
 import { authGuard } from "@bcwdev/auth0-vue"
 import VueRouter from "vue-router"
